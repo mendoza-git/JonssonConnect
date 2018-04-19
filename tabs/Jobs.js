8d56f0e482b0a8488c668974393277e3eb470b3a
Enrique Mendoza <mendoza.getmail@gmail.com>
2018-04-18 23:09:37 -0500

    Linking issue on jobs Tab fixed.

--- tabs/Jobs.js
+++ tabs/Jobs.js
@@ -4,7 +4,7 @@
  * @flow
  */
  import React, { Component } from 'react';
- import { ActivityIndicator, Image, ListView, FlatList, StyleSheet, View, TextInput, ImageBackground } from 'react-native';
+ import { ActivityIndicator, Image, ListView, FlatList, StyleSheet, View, Linking, TextInput, ImageBackground } from 'react-native';
  import { TabNavigator, StackNavigator } from "react-navigation";
  import { Container, Header, Content, Card, CardItem, Thumbnail, List, ListItem, Icon, Item, Input, Text, Title, Button, Left, Body, Right, H1, H2, H3 } from 'native-base';
  import * as firebase from 'firebase';
